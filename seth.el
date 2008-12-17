c4cc43ccf83833d05f5a665dd5286dc4f0e7b55f
Seth Falcon <seth@evri.com>
2008-12-16 21:47:59 -0800

    Add user seth customization file

---
+++ seth.el
@@ -0,0 +1,6 @@
+(setq tool-bar-mode nil)
+(setq default-frame-alist 
+      '((width . 85) (height . 33)
+        (font
+         .
+         "-apple-monaco-medium-r-normal--18-140-72-72-m-140-mac-roman")))
